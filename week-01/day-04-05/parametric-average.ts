9b83af989a222db422307f198135454978a945d6
benebp <benebp@github.com>
2019-12-05 17:22:08 +0100

    another 5 practises

---
+++ week-01/day-04-05/parametric-average.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number = 5;
+
+for (let i = 0; i <= input; i++) {
+    
+}
