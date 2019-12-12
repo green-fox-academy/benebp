c2d4a3918656e54e2e91ecb7cb2bc4ed8223b364
benebp <benebp@github.com>
2019-12-12 13:59:14 +0100

    week-02 day-04 purple steps 3d

---
+++ week-02/day-04/purple-steps-3d/purple-steps-3d-ts.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+const width = 1600;
+const height = 800;
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r4.png]
+
+for (let i: number = 0; i < 250; i += 50) {
+    let start: number = 0;
+    let side: number = 0;
+    side += i;
+    start = i + side;
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(start, start, side, side);
+    // ctx.strokeRect(0 + i, 0 + i, 50, 50);
+}
