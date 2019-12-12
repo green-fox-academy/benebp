678b8164f345f9283bdc6582360540b99fa914ba
benebp <benebp@github.com>
2019-12-12 11:15:53 +0100

    week-02 day-04 middle lines and colored box

---
+++ week-02/day-04/colored-box/colored-box-ts.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+const width = 1600;
+const heigth = 800;
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// draw a box that has different colored lines on each edge.
+
+ctx.strokeStyle = 'green';
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(500, 100);
+ctx.stroke();
+
+ctx.strokeStyle = 'red';
+ctx.beginPath();
+ctx.moveTo(500, 100);
+ctx.lineTo(500, 600);
+ctx.stroke();
+
+ctx.strokeStyle = 'yellow';
+ctx.beginPath();
+ctx.moveTo(500, 600);
+ctx.lineTo(100, 600);
+ctx.stroke();
+
+ctx.strokeStyle = 'blue';
+ctx.beginPath();
+ctx.moveTo(100, 600);
+ctx.lineTo(100, 100);
+ctx.stroke();
---
+++ week-02/day-04/middle-lines/middle-lines.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const width = 1600;
+const heigth = 800;
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
+
+ctx.strokeStyle = 'red';
+ctx.beginPath();
+ctx.moveTo(600, 400);
+ctx.lineTo(1000, 400);
+ctx.stroke();
+
+ctx.strokeStyle = 'green';
+ctx.beginPath();
+ctx.moveTo(800, 200);
+ctx.lineTo(800, 600);
+ctx.stroke();
+
+
+// for (let i = 50; i < 500; i += 100) {
+// 	if (i === 250) {
+// 		ctx.fillRect(i, i, 30, 30);
+// 	} else {
+// 		ctx.strokeRect(i, i, 30, 30);
+// 	}
+// }
+
+// function drawSquare(xPos: number, yPos: number, size: number, color: string): void {
+// 	ctx.strokeStyle = color;
+// 	ctx.strokeRect(xPos, yPos, size, size);
+// }
+
+// drawSquare(500, 500, 100, 'rgb(200,200,200)');
