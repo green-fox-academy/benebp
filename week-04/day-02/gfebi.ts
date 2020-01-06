ba814e03fdceddf77981afe6abc21699bc334dad
benebp <benebp@github.com>
2020-01-06 19:23:51 +0100

    week04 day01 class for next day mod

--- week-04/day-02/gfebi.ts
+++ week-04/day-02/gfebi.ts
@@ -1,3 +1,5 @@
+'use strict';
+
 class Person {
   name: string;
   age: number;
@@ -17,3 +19,113 @@ class Person {
     console.log('My goal is: Live for the moment!');
   }
 }
+
+class Student {
+  name: string;
+  age: number;
+  gender: string;
+  previousOrganization: string;
+  skippedDays: number;
+
+  constructor (name: string = 'Jane Doe', age: number = 30, gender: string = 'female', previousOrganization: string = 'The School of Life', skippedDays: number = 0) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = skippedDays;
+  }
+
+  introduce (): void {
+    console.log('Hi, I am ' + this.name + ', a' + this.age + 'year old ' + this.gender + 'from ' + this.previousOrganization + 'who skipped ' + this.skippedDays + 'days from the course already.');
+  }
+
+  getGoal (): void {
+    console.log('Be a junior software developer.');
+  }
+
+  skipDays (numberOfDays: number): void {
+    this.skippedDays + numberOfDays;
+  }
+}
+
+class Mentor {
+  name: string;
+  age: number;
+  gender: string;
+  level: string;
+
+  constructor (name: string = 'Jane Doe', age: number = 30, gender: string = 'female', level:string = 'intermediate') {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.level = level;
+  }
+
+  introduce (): void {
+    console.log('Hi, I am ' + this.name + ', a' + this.age + 'year old ' + this.gender + this.level + ' mentor.');
+  }
+
+  getGoal (): void {
+    console.log('Educate brilliant junior software developers.');
+  }
+}
+
+class Sponsor {
+  name: string;
+  age: number;
+  gender: string;
+  company: string;
+  hiredStudents: number;
+
+  constructor (name: string = 'Jane Doe', age: number = 30, gender: string = 'female', company: string = 'Google', hiredStudents: number = 0) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.company = company;
+    this.hiredStudents = hiredStudents;
+  }
+
+  introduce (): void {
+    console.log('Hi, I am ' + this.name + ', a' + this.age + 'year old ' + this.gender + 'who represents ' + this.company + ' and hired ' + this.hiredStudents + ' students so far.');
+  }
+
+  getGoal (): void {
+    console.log('Hire brilliant junior software developers.');
+  }
+
+  hire (): void {
+    this.hiredStudents++;
+  }
+}
+
+let people = [];
+let mark = new Person('Mark', 46, 'male');
+people.push(mark);
+let jane = new Person();
+people.push(jane);
+let john = new Student('John Doe', 20, 'male', 'BME');
+people.push(john);
+let student = new Student();
+people.push(student);
+let gandhi = new Mentor('Gandhi', 148, 'male', 'senior');
+people.push(gandhi);
+let mentor = new Mentor();
+people.push(mentor);
+let sponsor = new Sponsor();
+let elon = new Sponsor('Elon Musk', 46, 'male', 'SpaceX');
+people.push(elon);
+
+student.skipDays(3);
+
+for (let i = 0; i < 6; i++) {
+  elon.hire();
+}
+
+for (let i = 0; i < 4; i++) {
+  sponsor.hire();
+}
+
+people.forEach((person) => {
+  person.introduce();
+  person.getGoal();
+});
