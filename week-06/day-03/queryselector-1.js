255951cbdd8ef60a5540dacd897d0d29ac7ef23b
benebp <benebp@github.com>
2020-01-22 14:06:00 +0100

    week6 day3 queryselector1

---
+++ week-06/day-03/queryselector-1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let king = document.getElementById('b325').innerText;
+console.log(king);
+
+let businessLamp = document.querySelectorAll('.big');
+console.log(businessLamp[0].innerText);
+console.log(businessLamp[1].innerText);
+
+let conceitedKing = document.querySelector('.container').children;
+alert(conceitedKing[0].innerText);
+alert(conceitedKing[1].innerText);
+
+let noBusiness = document.querySelectorAll('div');
+console.log(noBusiness[0].innerText);
+console.log(noBusiness[1].innerText);
+console.log(noBusiness[2].innerText);
