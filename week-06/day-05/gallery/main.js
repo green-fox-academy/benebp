62502c5189c8cb0aa37b892221caff33594a6691
benebp <benebp@github.com>
2020-01-24 16:29:20 +0100

    gallery thumbnails incomplete

--- week-06/day-05/gallery/main.js
+++ week-06/day-05/gallery/main.js
@@ -4,7 +4,9 @@ window.onload = function() {
   const pics = [ { path: "pics/green.jpeg", title: "Lots of green", description: "We have so many things that we have to do better..." },
                 { path: "pics/blue.jpeg", title: "Lots of blue", description: "Some people have an ability to write placeholder text..." },
                 { path: "pics/grey.jpeg", title: "Lots of grey", description: "I think my strongest asset maybe by far is my temperament." },
-                { path: "pics/sephia.jpeg", title: "Lots of mixed... maybe sephia?", description: "Lorem Ipsum is unattractive, both inside and out." } ];
+                { path: "pics/sephia.jpeg", title: "Lots of mixed... maybe sephia?", description: "Lorem Ipsum is unattractive, both inside and out." },
+                { path: "pics/greener.jpeg", title: "More green stuff", description: "My text is long and beautiful." },
+                { path: "pics/white.jpeg", title: "Some white stuff too", description: "Lorem Ipsum is FAKE TEXT!" } ];
 
   const changePic = (picNum) => {
     document.getElementById('pic').src = pics[picNum].path;
@@ -34,4 +36,13 @@ window.onload = function() {
     console.log(picNum);
     changePic(picNum);
   };
+/*
+  pics.forEach(e => {
+    let ulElement = document.querySelector('.thumbnails');
+    let newThumbnail = document.createElement('li');
+    let newImg = document.createElement('img');
+    ulElement.appendChild(newThumbnail);
+    newThumbnail.appendChild(newImg);
+    document.querySelector('img').setAttribute('src', pics[0].path);
+  }); */
 };
