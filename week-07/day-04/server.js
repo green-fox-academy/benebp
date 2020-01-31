df76db281fe1b101af07f14cedd0a5d7816a6060
benebp <benebp@github.com>
2020-01-31 12:14:29 +0100

    bookstore filter

--- week-07/day-04/server.js
+++ week-07/day-04/server.js
@@ -29,10 +29,32 @@ app.get('/booktitles', (req, res) => {
   });
 });
 
+function queryFilter(query) {
+  let string = 'WHERE '
+  let columns = Object.keys(query);
+  let filters = Object.values(query);
+  for (let i = 0; i < columns.length; i++) {
+    if (columns[i] === 'category') {
+      string += `cate_descrip = '${filters[i]}' AND `;
+    };
+    if (columns[i] === 'publisher') {
+      string += `pub_name = '${filters[i]}' AND `;
+    };
+    if (columns[i] === 'plt') {
+      string += `book_price < '${filters[i]}' AND `;
+    };
+    if (columns[i] === 'pgt') {
+      string += `book_price > '${filters[i]}' AND `;
+    }
+  };
+  return string.slice(0, -5)
+};
+
 //join author - aut_name, book_mast - book_name, book_price, category - cat_descrip, publisher - pub_name
 app.get('/bookfulldata', (req, res) => {
-  let category = req.query.category;
-  let sql = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id WHERE cate_descrip = "${category}"`;
+  let query = req.query;
+  console.log(queryFilter(query));
+  let sql = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id ${queryFilter(query)}`;
   conn.query(sql, (err, rows) => {
     if (err) {
       console.error(`Cannot retrieve data: ${err.toString()}`);
