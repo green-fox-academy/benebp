f0bed3ef939699cb3a867ef308b0e9a472f85acc
benebp <benebp@github.com>
2020-02-03 14:50:38 +0100

    reddit clone backend make post

--- week-08/day-01/reddit/server.js
+++ week-08/day-01/reddit/server.js
@@ -22,7 +22,14 @@ app.get('/hello', (req, res) => {
   res.send('hello maunika');
 });
 
+function makeJSON(rows) {
+  let json = { posts: [] };
+  json.posts = [...rows];
+  return json
+}
+
 app.get('/posts', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
   let sql = `SELECT * FROM posts`;
   conn.query(sql, (err, rows) => {
     if (err) {
@@ -30,7 +37,33 @@ app.get('/posts', (req, res) => {
       res.sendStatus(500);
       return null;
     }
-    return res.status(200).json(rows);
+    return res.status(200).json(makeJSON(rows));
+  });
+});
+
+app.post('/posts', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  let title = req.body.title;
+  let url = req.body.url;
+  let timestamp = req.body.timestamp;
+  let sql = `INSERT INTO posts (title, url, timestamp) VALUES (?, ?, ?);`;
+  conn.query(sql, [title, url, timestamp], (err, rows) => {
+    let justPostedID = rows.insertId;
+    let sqlJustPosted = `SELECT * FROM posts WHERE id = ${justPostedID}`;
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+      return null;
+    } else {
+      conn.query(sqlJustPosted, (err, rows2) => {
+        if (err) {
+          console.error(`Cannot retrieve data: ${err.toString()}`);
+          res.sendStatus(500);
+          return null;
+        }
+        return res.status(200).json(rows2);
+      });
+    };
   });
 });
 
