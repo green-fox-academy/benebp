775f1447787977a6ad5e09b13afb3c10f5173c12
benebp <benebp@github.com>
2020-02-22 20:07:00 +0100

    todo app backend working

--- week-08/day-05/todo/app.js
+++ week-08/day-05/todo/app.js
@@ -21,7 +21,61 @@ conn.connect((err) => {
   console.log('Connection established');
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
+});
 
+app.get('/todos', (req, res) => {
+  const getTodosSQL = 'SELECT * FROM todos';
+  conn.query(getTodosSQL, (err, result) => {
+    if (err) {
+      res.send({ error: 'DB ERROR' });
+    } else {
+      res.send({ todos: result });
+    };
+  });
+});
 
+app.post('/todos', (req, res) => {
+  const newTodo = req.body.todo;
+  const newTodoSQL = 'INSERT INTO todos (todo) VALUES (?);';
+  if (newTodo) {
+    conn.query(newTodoSQL, [newTodo], (err) => {
+      if (err) {
+        res.send({ error: 'DB ERROR' });
+      } else {
+        res.send({ todo: 'CREATED' });
+      };
+    });
+  };
+});
+
+app.put('/todos/:id', (req, res) => {
+  const statusChangedTodoId = req.params.id;
+  const statusChangerSQL = 'UPDATE todos SET status = 1 WHERE id = ?;';
+  if (statusChangedTodoId) {
+    conn.query(statusChangerSQL, [statusChangedTodoId], (err) => {
+      if (err) {
+        res.send({ error: 'DB ERROR' });
+      } else {
+        res.send({ status: 'CHANGED' });
+      };
+    });
+  };
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const deletedTodoId = req.params.id;
+  const deleteSQL = 'DELETE FROM todos WHERE id = ?;';
+  if (deletedTodoId) {
+    conn.query(deleteSQL, [deletedTodoId], (err) => {
+      if (err) {
+        res.send({ error: 'DB ERROR' });
+      } else {
+        res.send({ todo: 'DELETED' }); 
+      };
+    });
+  };
+});
 
 app.listen(3000);
