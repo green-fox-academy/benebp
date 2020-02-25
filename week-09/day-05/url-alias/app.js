b9fb6b3fb15f10b0aabc0105f2870228c6093acb
benebp <benebp@github.com>
2020-02-25 20:41:40 +0100

    url aliaser app backend mod post endpoint incomplete

--- week-09/day-05/url-alias/app.js
+++ week-09/day-05/url-alias/app.js
@@ -43,7 +43,7 @@ app.get('/a/:alias', (req, res) => {
     const urlOfSpecificAlias = result[0].url;
     if (err) {
       res.send({ error: 'DB ERROR' });
-      //404 status if the alias not exists, but how??? another else if?
+      //404 status if the alias not exists, but how??? another else if? nem lehet ures arraykent venni
     } else {
       const incrementHitcountSQL = `UPDATE links SET hitcount = hitcount + 1 WHERE alias = ?;`;
       conn.query(incrementHitcountSQL, [specificAlias], (err) => {
@@ -57,11 +57,35 @@ app.get('/a/:alias', (req, res) => {
   });
 });
 
-// app.post('/api/links', (req, res) => {
-
-// });
+app.post('/api/links', (req, res) => {
+  const newUrl = req.body.url;
+  const newAlias = req.body.alias;
+  const aliasCheckSQL = 'SELECT * FROM links WHERE alias = ?;';
+  conn.query(aliasCheckSQL, [newAlias], (err, result) => {
+    console.log(JSON.stringify(result));
+    if (err) {
+      res.send({ error: 'DB ERROR' });
+      //kurva anyjat hogz miert nem ismeri fel h ures
+    } else if (JSON.stringify(result) != []) {
+      res.sendStatus(400)
+    } else {
+      let newSecretCode = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+      const insertNewLineSQL = 'INSERT INTO links (url, alias, secretcode) VALUES (?, ?, ?);';
+      conn.query(insertNewLineSQL, [newUrl, newAlias, newSecretCode], (err) => {
+        if (err) {
+          res.send({ error: 'DB ERROR' });
+        } else {
+          //majd a krealt ujat kell visszaadni
+          res.send('ok');
+        };
+      });
+    };
+  });
+});
 
 // app.delete('/api/links/:id', (req, res) => {
+//   const specificId = req.params.id;
+//   const specificSecretCode = req.body.secretcode;
 
 // });
 
